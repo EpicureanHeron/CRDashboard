2fa825485beac4b70e14bb066066abb016204f90
Joe Thor <joethor1989@gmail.com>
2019-03-18 09:03:57 -0500

    added archive folder locally and made a note on the sheet selection

--- dashboard.py
+++ dashboard.py
@@ -8,7 +8,7 @@ import csv
 wb = openpyxl.load_workbook('expense_analysis.xlsx')
 
 
-
+# change this to select the only sheet, which I have previously renamed as 'data' but it is usually some time stamp, think there is a notion of 'active' to select
 sheet = wb['Data']
 
 print('Opening workbook')
